076a2667ccd5a00f860c67b0176dabaf2dce6408
fdou <belithco@gmail.com>
2016-03-03 10:59:47 -0800

    Many changes
    Null nMap so that we can test if set.
    Add handler code for UI update.
    Class vars to cache dot and ISS rocket icons.
    Track prev/last marker so that we can convert from rocket to breadcrumb.
    Add start of service triggering.
    Implement onResume():
    - Cache dot and ISS rocket icons.
    - Define handler: recv long/lat; breadcrumb logic; place rocket; move 'camera'
    Remove default toy Sydney marker.

--- app/src/main/java/com/belithco/iss/isswatch/ISSMapActivity.java
+++ app/src/main/java/com/belithco/iss/isswatch/ISSMapActivity.java
@@ -1,18 +1,30 @@
 package com.belithco.iss.isswatch;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class ISSMapActivity extends FragmentActivity implements OnMapReadyCallback {
+    public static final String ISSWATCH = "ISSWatch:";
+    private static final String TAG = ISSWATCH + ISSMapActivity.class.getSimpleName();
 
-    private GoogleMap mMap;
+    private GoogleMap mMap = null;
+    public static Handler handler = null;
+    private BitmapDescriptor issIcon = null;
+    private BitmapDescriptor dotIcon = null;
+    Marker markerPrv = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +34,35 @@ public class ISSMapActivity extends FragmentActivity implements OnMapReadyCallba
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        // Start our polling service
+        ISSLocationService.startServiceTriggering(getApplicationContext());
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        issIcon = BitmapDescriptorFactory.fromResource(R.drawable.rocket64);
+        dotIcon = BitmapDescriptorFactory.fromResource(R.drawable.dot);
+        handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                Bundle reply = msg.getData();
+                double latitude = reply.getDouble(ISSLocationService.LATITUDE);
+                double longitude = reply.getDouble(ISSLocationService.LONGITUDE);
+                Log.d(TAG, "Lat: "+latitude+" long: "+longitude);
+                // Add a marker in Sydney and move the camera
+                LatLng issPosition = new LatLng(latitude, longitude);
+                if (null!=issIcon) {
+                    if (null!=markerPrv) {
+                        markerPrv.setIcon(dotIcon);
+                    }
+                    MarkerOptions markerOpt = new MarkerOptions().position(issPosition).title("ISS Location").icon(issIcon);
+                    markerPrv = mMap.addMarker(markerOpt);
+                }
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(issPosition));
+            }
+        };
+    }
 
     /**
      * Manipulates the map once available.
@@ -37,10 +76,5 @@ public class ISSMapActivity extends FragmentActivity implements OnMapReadyCallba
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
